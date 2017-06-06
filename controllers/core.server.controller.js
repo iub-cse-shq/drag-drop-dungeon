81cbe1e15c33a2b84400a238cf545a0f99049dc2
Shama Hoque <shama.hoque@gmail.com>
2017-06-06 05:41:24 +0000

    index and make initial views added

--- controllers/core.server.controller.js
+++ controllers/core.server.controller.js
@@ -4,7 +4,7 @@
  * Module dependencies.
  */
 exports.index = function(req, res) {
-	res.render('./../index.ejs', {
+	res.render('./../public/views/index.ejs', {
 		user: req.user || null,
 		request: req
 	});
@@ -12,6 +12,13 @@ exports.index = function(req, res) {
 
 exports.about = function(req, res) {
 	res.render('./../about.ejs', {
+		user: req.user || null,
+		request: req
+	});
+};
+
+exports.make = function(req, res) {
+	res.render('./../public/views/make.ejs', {
 		user: req.user || null,
 		request: req
 	});
